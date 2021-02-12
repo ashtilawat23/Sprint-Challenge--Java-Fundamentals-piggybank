d5fcdb918e7fb31d740a8639f4fadf32d0de6efe
ashtilawat23 <ashtilawat23@gmail.com>
2021-02-12 12:40:35 -0500

    MVP complete, endpoint works.

--- src/main/java/com/lambdaschool/piggybank/PiggyBankApplication.java
+++ src/main/java/com/lambdaschool/piggybank/PiggyBankApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// For the app
 public class PiggyBankApplication
 {
 
